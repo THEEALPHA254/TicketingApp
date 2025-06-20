9b057d22d456da32ad0b4193331bc60b0c6edbc4
THEEALPHA254 <gracemuriithi2004@gmail.com>
2025-06-20 12:52:49 +0300

    Added user types

--- log_in/settings.py
+++ log_in/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'login_app',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3001",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'log_in.urls'
 
--- login_app/models.py
+++ login_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # custom user model
 class User(models.Model):
+    user_type = models.CharField(max_length=255, blank=True, null=True)
     username = models.CharField(max_length=150, unique=True)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
